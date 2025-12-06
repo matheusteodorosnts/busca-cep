1bc00b6bae1482c7774945824b124a00c3abbddc
matheusteodorosnts <matheusteodorodossantos2013@gmail.com>
2025-12-06 20:30:10 -0300

    feat(api): add react-query setup

---
+++ src/hooks/useGetCep.tsx
@@ -0,0 +1,18 @@
+import { CepType } from "@/types/cep";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export default function useGetCep(cep: string) {
+	async function getCep() {
+		const { data } = await axios.get<CepType>(
+			`https://viacep.com.br/ws/${cep}/json/`,
+		);
+		return data;
+	}
+
+	return useQuery({
+		queryKey: ["cep", cep],
+		queryFn: getCep,
+		enabled: cep.length === 8,
+	});
+}
---
+++ src/types/cep.ts
@@ -0,0 +1,9 @@
+export type CepType = {
+	cep: string;
+	logradouro: string;
+	bairro: string;
+	estado: string;
+	uf: string;
+	ibge: string;
+	ddd: number;
+};
